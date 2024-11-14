2e23e926ffc64bf4fed6b0dff4551c2da9d78da7
pratik2405 <shinde7388pratik@gmail.com>
2024-11-14 13:01:28 +0530

    Add project files.

---
+++ Assignment_Event_Delegate/Bank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Event_Delegate
+{
+    //Create a Bank a class, with Debit & Credit method
+    //You can assign a default balance using constructor
+    //Credit method will accept the amount that needs to be added in balance
+    //Debit method will accept the amount that needs to be deducted from the balance.
+    public class Bank
+    {
+        private int bal;
+        public event DisplayTransection Successful;
+        public event DisplayTransection InsufficientBal;
+        public event DisplayTransection LowBal;
+        public event DisplayTransection ZeroBal;
+
+        public Bank(int bal)
+        {
+            this.bal = bal;    
+        }
+
+        public void Credit(int amt)
+        {
+            bal = bal + amt;
+        }
+
+        public void Debit(int amt)
+        {
+            if(bal == 0)
+            {
+                ZeroBal();
+            }
+            else if ( bal - amt < 3000)
+            {
+                bal = bal - amt;
+                InsufficientBal();
+            }
+            else if (amt > bal)
+            {
+                LowBal();
+            }
+            else
+            {
+                bal = bal - amt;
+                Successful();
+            }
+            
+        }
+    }
+}
---
+++ Assignment_Event_Delegate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Event_Delegate
+{
+    //Create a Bank a class, with Debit & Credit method
+    //You can assign a default balance using constructor
+    //Credit method will accept the amount that needs to be added in balance
+    //Debit method will accept the amount that needs to be deducted from the balance.
+    //If amount is greater than balance raise an event → insufficient balance
+    //If balance is low (< 3000) raise an event low balance
+    //If balance is zero then raise an event zero balance.
+
+    public delegate void DisplayTransection();
+    public class Program
+    {
+        static void SuccessfulMsg()
+        {
+            Console.WriteLine("Transaction is Successfull");
+        }
+        static void InsufficientBalMsg()
+        {
+            Console.WriteLine("Balance is insufficient");
+        }
+        static void LowBalMsg()
+        {
+            Console.WriteLine("Minimum 3000 balance is required ! ");
+            Console.WriteLine("Low Balance !");
+        }
+        static void ZeroBalMsg()
+        {
+            Console.WriteLine("Balance is Zero/0");
+        }
+        static void Main(string[] args)
+        {
+            Bank b1 = new Bank(0);
+            b1.Successful += new DisplayTransection(SuccessfulMsg);
+            b1.LowBal += new DisplayTransection(LowBalMsg);
+            b1.InsufficientBal += new DisplayTransection(InsufficientBalMsg);
+            b1.ZeroBal += new DisplayTransection(ZeroBalMsg);
+
+            //b1.Credit(2000);
+
+            //b1.Debit(1000);
+        }
+    }
+}
